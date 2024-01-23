9a74da493ba6b03e30739e6157b784623b27076a
nelson ejali <ejali267@gmail.com>
2024-01-23 16:25:56 +0100

    added a reusable component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} star</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating maxRating={5} defaultRating={3} />
+    <StarRating
+      maxRating={5}
+      size={24}
+      messages={['Terrible', 'Bad', 'Okay', 'good', 'amazing']}
+    />
+    <Test />
   </React.StrictMode>
 );
